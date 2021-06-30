aeb8adf2ee22549fe2836b17929b2020fa33a549
leccorside <leccorside@gmail.com>
2021-06-30 20:15:51 -0300

    intro, login, esqueci senha, cadastro e conta criada, abrir arquivos

--- src/app/cadastro4/cadastro4.page.ts
+++ src/app/cadastro4/cadastro4.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { ActionSheetController, NavController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-cadastro4',
@@ -8,7 +9,10 @@ import { NavController } from '@ionic/angular';
 })
 export class Cadastro4Page implements OnInit {
 
-  constructor(private navCtrl: NavController) { }
+  constructor(
+    private navCtrl: NavController,
+    public actionSheetCtrl: ActionSheetController
+    ) { }
 
   ngOnInit() {
   }
@@ -20,5 +24,39 @@ export class Cadastro4Page implements OnInit {
   proximo(){
     this.navCtrl.navigateRoot('/contacriada');
   }
+
+  abrircamera(){
+    console.log('Camera');
+  }
+
+  abrirarquivo(){
+    console.log('Galeria');
+  }
+
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Selecione um arquivo',
+      cssClass: 'my-custom-class',
+      buttons: [{
+        text: 'Câmera',
+        icon: 'camera',
+        handler: () => {
+          this.abrircamera();
+        }
+      }, {
+        text: 'Galeria',
+        icon: 'image',
+        handler: () => {
+          this.abrirarquivo();
+        }
+      }]
+    });
+    await actionSheet.present();
+
+    const { role } = await actionSheet.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+
 
 }
